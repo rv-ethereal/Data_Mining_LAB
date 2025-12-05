b219dd98ecdab3b710e4b9744205588ccb09593c
Kasi Chebrolu <kasichebrolu369@gmail.com>
2025-12-05 23:23:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,3 @@
+from superset.app import create_app
+
+app = create_app()
---
+++ superset_config.py
@@ -0,0 +1,5 @@
+# Superset Configuration File
+import os
+
+# Disable SQLite security restrictions
+PREVENT_UNSAFE_DB_CONNECTIONS = False
